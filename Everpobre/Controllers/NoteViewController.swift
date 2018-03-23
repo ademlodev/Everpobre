8e7e85b63364b42e7a44341b213a2d9ef2b8b7fb
Javier Garcia <javier.garcia.olmeda@gmail.com>
2018-03-23 01:19:01 +0100

    initial NoteVC

---
+++ Everpobre/Controllers/NoteViewController.swift
@@ -0,0 +1,24 @@
+//
+//  NoteViewController.swift
+//  Everpobre
+//
+//  Created by Javi on 23/3/18.
+//  Copyright © 2018 Javi. All rights reserved.
+//
+
+import UIKit
+
+class NoteViewController:  UIViewController{
+    
+    override func loadView() {
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    
+}
